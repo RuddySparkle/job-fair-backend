48de0366a724095452dec653156593361672de41
Rutthee Rawatkhunanon <rutthee9999@gmail.com>
2023-11-12 23:28:43 +0700

    update booking controller

--- controllers/bookings.js
+++ controllers/bookings.js
@@ -34,11 +34,17 @@ exports.getBookings = async (req, res, next) => {
       query = Booking.find({ company: companyId }).populate({
         path: "company",
         select: "name address tel",
+      }).populate({
+        path: "user",
+        select: "name email tel",
       });
     } else {
       query = Booking.find().populate({
         path: "company",
         select: "name address tel",
+      }).populate({
+        path: "user",
+        select: "name email tel",
       });
     }
   }
@@ -67,6 +73,9 @@ exports.getBooking = async (req, res, next) => {
     booking = await Booking.findById(req.params.id).populate({
       path: "company",
       select: "name address tel",
+    }).populate({
+      path: "user",
+      select: "name email tel",
     });
     
     if (!booking) {
